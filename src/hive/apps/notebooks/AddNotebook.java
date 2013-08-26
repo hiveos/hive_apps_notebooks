718c11879a69936efed900e7da5420db941eb22f
Ihsan Isik <x.ihsan@gmail.com>
2013-08-26 14:04:30 +0200

    Added background and action bar textures, altered notebook adding activity design, changed some action bar icons, removed notebook editText background (the blue bar at the bottom), new resources...

--- src/hive/apps/notebooks/AddNotebook.java
+++ src/hive/apps/notebooks/AddNotebook.java
@@ -1,19 +1,63 @@
 package hive.apps.notebooks;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddNotebook extends Activity {
+
+	Button doneButton;
+	Shelf shelfObject;
+	EditText notebookName;
+	public String actualNotebookName;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.addnotebook);
+		notebookName = (EditText) findViewById(R.id.notebookNameId);
+		shelfObject = new Shelf();
+
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 		
+		Spinner nbbgstyle = (Spinner)findViewById(R.id.notebookbgtype);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.notebook_styles, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		nbbgstyle.setAdapter(adapter);
 	}
-	
-	
-	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.addnotebook, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.action_savenotebook:
+			actualNotebookName = notebookName.getText().toString();
+			super.onBackPressed();
+			Shelf.someButton.setText(actualNotebookName);
+			Shelf.someButton.setId(shelfObject.notebookCounter);
+			return true;
+		default:
+			return false;
+
+		}
+
+	}
+
+
 
 }
--- src/hive/apps/notebooks/Shelf.java
+++ src/hive/apps/notebooks/Shelf.java
@@ -1,19 +1,48 @@
 package hive.apps.notebooks;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
-public class Shelf extends Activity {
-
+public class Shelf extends Activity implements OnClickListener {
+	
+	public LinearLayout ll;
+	public LayoutParams lp;
+	public static Button someButton;
+	public int notebookCounter;
+	public Context kontekst;
+	AddNotebook dodajSveskuObjekt;
+	
+	public void dodajSvesku(){
+		someButton = new Button(this);
+		someButton.setText("Notebook"+notebookCounter);
+		notebookCounter++;
+		ll = (LinearLayout)findViewById(R.id.shelfId);
+		lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		ll.addView(someButton,lp);
+		someButton.setText(dodajSveskuObjekt.actualNotebookName);
+		someButton.setOnClickListener(this);
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.shelf);
+		dodajSveskuObjekt = new AddNotebook();
+		notebookCounter=0;
+		
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(false);
 
 	}
 
@@ -31,6 +60,7 @@ public class Shelf extends Activity {
 
 		switch (item.getItemId()) {
 		case R.id.action_addnotebook:
+			dodajSvesku();
 			Intent AddNotebook = new Intent(this, AddNotebook.class);
 			startActivity(AddNotebook);
 			return true;
@@ -43,9 +73,16 @@ public class Shelf extends Activity {
 
 	// Static code to show second activity (Notebook Edit)
 	public void gotoNotebook(View View) {
-
 		Intent gotoNotebookInt = new Intent(this, Glavna.class);
 		startActivity(gotoNotebookInt);
 	}
 
-}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent gotoNotebookInt = new Intent(this, Glavna.class);
+		startActivity(gotoNotebookInt);
+		}
+		
+	}
+
