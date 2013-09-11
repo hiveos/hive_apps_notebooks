d2b7f551dd8a1e6a4d7454ca5b81b34c8a9cd2a0
Ihsan Isik <x.ihsan@gmail.com>
2013-09-11 16:53:13 +0200

    Added TextView to show notebook name on cover in real-time in AddNotebook activity

--- src/hive/apps/notebooks/AddNotebook.java
+++ src/hive/apps/notebooks/AddNotebook.java
@@ -3,18 +3,23 @@ package hive.apps.notebooks;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddNotebook extends Activity {
 
 	Button doneButton;
 	Shelf shelfObject;
 	EditText notebookName;
+	TextView notebookNameDisplay;
 	public String actualNotebookName;
 
 	@Override
@@ -23,7 +28,10 @@ public class AddNotebook extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.addnotebook);
 		notebookName = (EditText) findViewById(R.id.notebookNameId);
+		notebookNameDisplay = (TextView) findViewById(R.id.notebook_name_on_cover);
 		shelfObject = new Shelf();
+
+		notebookName.addTextChangedListener(textWatcher);
 
 		ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
@@ -34,6 +42,37 @@ public class AddNotebook extends Activity {
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		nbbgstyle.setAdapter(adapter);
 	}
+
+	private TextWatcher textWatcher = new TextWatcher() {
+
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+			if (notebookName.getText().length() <= 11) {
+				notebookNameDisplay.setText(notebookName.getText());
+			}
+			
+			  if(notebookName.getText().length() > 11){ String
+			  namecontainer; namecontainer = notebookName.getText().toString();
+			  notebookNameDisplay.setText(namecontainer.substring(0, 11) + "...");
+			  
+			  }
+			 
+
+		}
+
+		@Override
+		public void afterTextChanged(Editable s) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+			// TODO Auto-generated method stub
+
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,9 +86,16 @@ public class AddNotebook extends Activity {
 		switch (item.getItemId()) {
 		case R.id.action_savenotebook:
 			actualNotebookName = notebookName.getText().toString();
-			super.onBackPressed();
-			Shelf.someButton.setText(actualNotebookName);
-			Shelf.someButton.setId(shelfObject.notebookCounter);
+			if (actualNotebookName == null) {
+				Toast toast = Toast.makeText(this,
+						R.string.error_empty_notebook_name, Toast.LENGTH_LONG);
+				toast.show();
+			} else {
+
+				super.onBackPressed();
+				Shelf.someButton.setText(actualNotebookName);
+				Shelf.someButton.setId(shelfObject.notebookCounter);
+			}
 			return true;
 		default:
 			return false;
